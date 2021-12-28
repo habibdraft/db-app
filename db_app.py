78a694fb11155d6311baece904c5bb15967484a8
Nazia Habib <habib.nazia@gmail.com>
2021-12-28 11:11:38 -0600

    Update db_app.py

--- db_app.py
+++ db_app.py
@@ -2,9 +2,10 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
+import config
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Flipako40@localhost'
+app.config['SQLALCHEMY_DATABASE_URI'] = config.postgresConn
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
