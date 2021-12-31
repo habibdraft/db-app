5607a6b6869b9662e4ac94cac8daf372e239aab3
Nazia Habib <habib.nazia@gmail.com>
2021-12-31 13:47:26 -0600

    Update and rename post_to_db_api.py to post_to_stations.py

--- post_to_stations.py
+++ post_to_stations.py
@@ -9,7 +9,10 @@ def xml_to_dict(element):
     if element.text == None and len(element.attrib):
         return element.tag, element.attrib
     return element.tag, dict(map(xml_to_dict, element)) or element.text
-  
+
+url = 'http://127.0.0.1:5000/stations/'
+headers = {"Content-Type": "application/json; charset=utf-8"}
+
 for station in stations:
     d = xml_to_dict(station)
     response = requests.post(url, headers=headers, json=d[1])
