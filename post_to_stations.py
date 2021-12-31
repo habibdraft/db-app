1595a8952db272a7fe43863ff6a84c2e5acc4a9c
Nazia Habib <habib.nazia@gmail.com>
2021-12-31 16:51:12 -0600

    Update post_to_stations.py

--- post_to_stations.py
+++ post_to_stations.py
@@ -2,13 +2,13 @@ import requests
 import json
 from bart_api import BartApi
 
-bart = BartApi()
-stations = bart.get_stations()
-
 def xml_to_dict(etree_elem):
     if etree_elem.text == None and len(etree_elem.attrib):
         return etree_elem.tag, etree_elem.attrib
     return etree_elem.tag, dict(map(xml_to_dict, etree_elem)) or etree_elem.text
+
+bart = BartApi()
+stations = bart.get_stations()
 
 url = 'http://127.0.0.1:5000/stations/'
 headers = {"Content-Type": "application/json; charset=utf-8"}
